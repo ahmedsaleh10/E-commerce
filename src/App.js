e71bdce4542a036a11ed5521e3f25e50f1acad73
Maher Nahla <Maher.Nahla@wb.paltel.net>
2023-04-20 20:11:08 +0200

    adding images public folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route index element={HomePage}/>
-        <Route path="/details" element={detailsPage}/>
+        <Route index element={ProductCard}/>
+        {/* <Route path="/details" element={detailsPage}/>
         <Route path="/about" element={AboutUs}/>
-        <Route path="*" element={NotFound}/>
+        <Route path="*" element={NotFound}/> */}
 
       </Routes>
     </BrowserRouter>
--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -4,45 +4,20 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
 
-
-
-
-// import CardMedia from "@mui/material/CardMedia";
-// import Button from '@mui/material/Button';
-
 import ProductPrice from "./ProductPrice";
 import ProductName from "./ProductName";
 import ProductRaing from "./ProductRating";
 import Badge from "./Badge";
 
-import {FavoriteBorderOutlinedIcon} from '@mui/icons-material';
-import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
-
-//  import FavoriteBorderOutlinedIcon from '@mui/icons/FavoriteBorderOutlined';
-// import VisibilityOutlinedIcon from '@mui/icons/VisibilityOutlined';
-
-
-
-// import CardHeader from "@mui/material/CardHeader";
-// import Avatar from "@mui/material/Avatar";
-// import IconButton from "@mui/material/IconButton";
-// import { red } from "@mui/material/colors";
-// import MoreVertIcon from "@mui/icons-material/MoreVert";
-
 const ProductCard = () => {
   return (
     <Card variant="outlined" sx={{ maxWidth: 270 }}>
       <Box className={styles.background}>
-        <Badge className={styles.badgePosition}
+        <Badge
+          className={styles.badgePosition}
           isNew={false}
           salePercentage={14}
         />
-
-<IconButton>
-      <FavoriteBorderOutlinedIcon />
-    </IconButton>
-
 
         <img
           className={styles.productImage}
@@ -53,7 +28,7 @@ const ProductCard = () => {
 
       <CardContent>
         <ProductName />
-       <ProductPrice price={120} oldPrice={160} />
+        <ProductPrice price={120} oldPrice={160} />
         <ProductRaing ratingValue="3.5" RatingTimes="88" />
       </CardContent>
     </Card>
