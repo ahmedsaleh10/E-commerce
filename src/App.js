081fefff45445e1374960759c3498d672e8054b3
sondosnaalwi <dheliehsondos@gmail.com>
2023-04-21 00:52:24 -0700

    created header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Header from "./Header/header";
+
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route index element={HomePage}/>
-        <Route path="/details" element={detailsPage}/>
-        <Route path="/about" element={AboutUs}/>
-        <Route path="*" element={NotFound}/>
-
-      </Routes>
-    </BrowserRouter>
+      <Header />
   );
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+
+  //       <Route index element={HomePage}/>
+  //       <Route path="/details" element={detailsPage}/>
+  //       <Route path="/about" element={AboutUs}/>
+  //       <Route path="*" element={NotFound}/>
+
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
