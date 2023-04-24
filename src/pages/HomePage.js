e85051dbe95a9d91c579fc29e0150b032c50b849
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-04-24 16:27:42 +0300

    rename Footer and Categories folders.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ import Footer from "../components/Footer/Footer";
 import FlashSales from "../components/FlashSales/FlashSales"
 import OurProduct from "../components/OurProduct/OurProduct"
 import Features from "../components/Features/Features"
-import Category from './../components/Categories/index';
+import Category from '../components/Categories/index';
 import BestSales from "../components/BestSales/BestSales";
 
 const HomePage = () => {
