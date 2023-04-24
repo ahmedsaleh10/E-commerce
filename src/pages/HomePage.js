01cf92c66a87aef9a96c491a9f4c36b1e13c5eac
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-04-24 17:37:47 +0300

    resolve names for components.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,11 @@ import Header from "../components/Header/header";
 import SliderSection from "../components/SliderSection/SliderSection";
 import NewArrival from "../components/NewArrival/NewArrival";
 import Music from "../components/Music/Music";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/footer/Footer";
 import FlashSales from "../components/FlashSales/FlashSales"
 import OurProduct from "../components/OurProduct/OurProduct"
 import Features from "../components/Features/Features"
-import Category from '../components/Categories/index';
+import Category from '../components/categories/index';
 import BestSales from "../components/BestSales/BestSales";
 
 const HomePage = () => {
