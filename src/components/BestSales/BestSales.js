2742c5076cb1f86f0a489f2ad808a75880e45ee0
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-04-30 11:42:22 +0300

    made BestSales section in hompage responsive

--- src/components/BestSales/BestSales.js
+++ src/components/BestSales/BestSales.js
@@ -2,38 +2,55 @@ import React from "react";
 import Subheader from "../SubHeader/Subheader";
 import Sectionheader from "../SectionHeader/Sectionheader";
 import { Box, Button, Container, Stack } from "@mui/material";
-import data from '../../data'
+import data from "../../data";
+import Grid from "@mui/material/Grid";
+
 import ProductCard from "../ProductCard";
 const BestSales = () => {
   return (
-    <Box  sx={{padding:'0px',margin:'0px 8.4375rem'}}>
-      <Stack mt={10}direction="row" justifyContent="space-between" alignItems="center">
+    <Container maxWidth="lg">
+      <Stack
+        mt={10}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Box>
           <Subheader title="this Month" />
           <Sectionheader title="Best Selling Products" />
         </Box>
-          <Button
+        <Button
           mb={5}
-            sx={{
-              backgroundColor: "#DB4444",
-              padding: "1rem 3rem",
-              color: "white",
-              fontFamily: "Poppins",
-              textTransform: "none",
-              "&:hover": { backgroundColor: "#DB4444", color: "white" },
-            }}
-          >
-            View All
-          </Button>
+          sx={{
+            backgroundColor: "#DB4444",
+            padding: "1rem 3rem",
+            color: "white",
+            fontFamily: "Poppins",
+            textTransform: "none",
+            "&:hover": { backgroundColor: "#DB4444", color: "white" },
+            "&:size": { xs: "small", lg: "large" },
+          }}
+        >
+          View All
+        </Button>
       </Stack>
-      <Stack justifyContent={"space-between"} flexDirection={"row"} mt={1} mb={20}>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing={{ xs: 3, sm: 2, md: 4 }}
+        columns={{ xs: 3.5, sm: 7, md: 12 }}
+      >
         {data
-          .filter((item) => item.rating.rate >4).slice(0,4)
+          .filter((item) => item.rating.rate > 4)
+          .slice(0, 4)
           .map((item, index) => (
-            <ProductCard key={index} product={item} />
+            <Grid item xs={3} sm={3} md={3} lg={1} key={index}>
+              <ProductCard product={item} />
+            </Grid>
           ))}
-      </Stack>
-    </Box>
+      </Grid>
+    </Container>
   );
 };
 
