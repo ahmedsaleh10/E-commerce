f1a5ca0a15eeae4a58ae36f07c729f2528a06044
Maher-N <MaherNahlaB@gmail.com>
2023-05-01 19:51:51 +0300

    fix features icon container style to be a circle by adding width equals the height

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Header/header'
 import Footer from '../components/footer/Footer'
 import Breadcrumb from '../components/BreadCrumb'
+import Features from '../components/Features/Features'
 
 const About = () => {
   return (
     <>
         <Header/>
         <Breadcrumb  path="Home/About"/>
+        <Features/>
         <Footer/>
 
     </>
